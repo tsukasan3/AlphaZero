2a58d084350cdbee30319bc467bf20466181b539
seiji <31160065+tsukasan3@users.noreply.github.com>
2020-08-22 18:05:11 +0900

    Add files via upload

---
+++ connect_four/human_play.py
@@ -0,0 +1,101 @@
+# coding: UTF-8
+ from game import State
+ from pv_mcts import pv_mcts_action
+ from tensorflow.keras.models import load_model
+ from pathlib import Path
+ from threading import Thread
+ import tkinter as tk
+
+ # ベストプレイヤーの読み込み
+ model = load_model('./model/best.h5')
+
+ # ゲームUIの定義
+ class GameUI(tk.Frame):
+     def __init__(self, master=None, model=None):
+         tk.Frame.__init__(self, master)
+         self.master.title('コネクトフォー')
+
+         # ゲーム状態の生成
+         self.state = State()
+
+         # PV MCTSで行動選択を行う関数の生成
+         self.next_action = pv_mcts_action(model, 0.0)
+
+         # キャンバスの生成
+         self.c = tk.Canvas(self, width=280, height=240, highlightthickness=0)
+         self.c.bind('<Button-1>', self.turn_of_human)
+         self.c.pack()
+
+         # 描画の更新
+         self.on_draw()
+
+    # 人間のターン
+    def turn_of_human(self, event):
+        # ゲーム終了時、ゲーム状態を初期状態に戻す
+        if self.state.is_done():
+            self.state() = State()
+            self.on_draw()
+            return
+
+        # 先手でない場合
+        if not self.state.is_first_player():
+            return
+
+        # クリックの位置を行動に変換
+        x = int(event.x / 40)
+        if x < 0 or 7 < x:
+            return
+        action = x
+
+        # 合法手でない場合
+        if not (action in self.state.legal_actions()):
+            return
+
+        # 次の状態の取得
+        self.state = self.state.next(action)
+        self.on_draw
+
+        # AIのターン
+        self.master.after(1, self.turn_of_ai)
+
+    # AIのターン
+    def turn_of_ai(self):
+        if self.state.is_done():
+            return
+
+        # 行動の取得
+        action = self.next_action(self.state)
+
+        # 次の状態の取得
+        self.state = self.state.next(action)
+        self.on_draw()
+
+    # 石の描画
+    def draw_piece(self, index, first_player):
+        x = (index % 7) * 40 + 5
+        y = int(index / 7) * 40 + 5
+        if first_player:
+            self.c.create_oval(x, y, x+30, y+30, width=1.0, fill='#FF0000')
+        else:
+            self.c.create_oval(x, y, x+30, y+30, width=1.0, fill='#FFFF00')
+
+    # 描画の更新
+    def on_draw(self):
+        self.c.delete('all')
+        self.c.create_rectangle(0, 0, 280, 240, width=0.0, fill = '#00A0FF')
+        for i in range(42):
+            x = (i % 7) * 40 + 5
+            y = int(i / 7) * 40 + 5
+            self.c.create_oval(x, y, x+30, y+30, width=1.0, fill='#FFFFFF')
+
+        for i in range(42):
+            if self.state.pieces[i] == 1:
+                self.draw_piece(i, self.state.is_first_player())
+            if self.state.enemy_pieces[i] == 1:
+                self.draw_piece(i, not self.state.is_first_player())
+
+
+# ゲームUIの実行
+f = GameUI(model=model)
+f.pack()
+f.mainloop()
